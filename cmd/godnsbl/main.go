72299ab572fe8dc67075cbcf034d10cdad6d9abb
kitkatayama <9929234+kitkatayama@users.noreply.github.com>
2023-01-13 11:11:04 +0900

    work with godnsbl targetDNS targetIP

--- cmd/godnsbl/main.go
+++ cmd/godnsbl/main.go
@@ -13,27 +13,30 @@ import (
 
 func main() {
 
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Println("Please supply a domain name or IP address.")
 		os.Exit(1)
 	}
 
-	ip := os.Args[1]
+	ip := os.Args[2]
+	bl := os.Args[1]
 
 	wg := &sync.WaitGroup{}
-	results := make([]godnsbl.Result, len(godnsbl.Blacklists))
-	for i, source := range godnsbl.Blacklists {
-		wg.Add(1)
-		go func(i int, source string) {
-			defer wg.Done()
-			rbl := godnsbl.Lookup(source, ip)
-			if len(rbl.Results) == 0 {
-				results[i] = godnsbl.Result{}
-			} else {
-				results[i] = rbl.Results[0]
-			}
-		}(i, source)
-	}
+	results := make([]godnsbl.Result, 1)
+	i := 0
+	source := bl
+	//for i, source := range godnsbl.Blacklists {
+	wg.Add(1)
+	go func(i int, source string) {
+		defer wg.Done()
+		rbl := godnsbl.Lookup(source, ip)
+		if len(rbl.Results) == 0 {
+			results[i] = godnsbl.Result{}
+		} else {
+			results[i] = rbl.Results[0]
+		}
+	}(i, source)
+	//}
 
 	wg.Wait()
 
